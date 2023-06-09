0daead7f9843603d1bfae22bf6a6542ac2786e71
tamhuynhoven <tamhuynh@oven.bz>
2023-06-09 15:37:45 +0700

    Add create a user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
   ],
-  controllers: [],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,13 @@
 import { Repository } from 'typeorm';
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ERole } from './enums/role.enum';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
@@ -13,5 +19,14 @@ export class UsersService {
 
   async findByEmail(email: string): Promise<UserEntity> {
     return this.userRepository.findOneBy({ email });
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const password = await argon2.hash(createUserDto.password);
+      return await this.userRepository.save({ ...createUserDto, role: ERole.Member, password });
+    } catch(e) {
+      throw new UnprocessableEntityException(e.detail);
+    }
   }
 }
